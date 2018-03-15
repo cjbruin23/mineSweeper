ded599c3970ea6d061824c7bcbe0f6db05bcb611
Colin Bruin <colinjb23@gmail.com>
2018-03-15 19:04:32 -0400

    creating a array map

--- sweep.js
+++ sweep.js
@@ -5,60 +5,90 @@ function Mineboard(rows, cols, mines) {
    this.numberOfMines = mines;
    this.heightOfBoard = cols;
    this.widthOfBoard = rows;
+   var map = [];
+
+   this.createBoardGrid = function() {
+
+      for (let i = 0; i < this.widthOfBoard; i++) {
+         let newRow = [];
+         for (let l = 0; l < this.heightOfBoard; l++) {
+            newRow.push([' ']);
+         };
+         map.push(newRow);
+      };
+      this.placeMines();
+   }
 
    // Functions
-   this.create = function() {
+   this.createBoard = function() {
 
       let board = document.createElement('div');
       board.setAttribute('id', 'board');
+      console.log(map);
 
-      for (let i = 0; i < this.widthOfBoard; i++) {
+      for (let i = 0; i < map.length; i++) {
          let row = document.createElement('div');
          row.className = 'row';
          board.appendChild(row);
-         for (let l = 0; l < this.heightOfBoard; l++) {
+         for (let l = 0; l < map[i].length; l++) {
             let cell = document.createElement('div');
-            cell.className = 'cell';
+            let checkIfEmpty = map[i][l].includes(" ")
+            if (checkIfEmpty) {
+               cell.className = 'cell';
+            } else {
+               cell.className = 'mine';
+            }
+
             cell.addEventListener("click", function(e) {self.revealCell();}, false);
             row.appendChild(cell);
          };
       };
       document.body.appendChild(board);
-      this.placeMines();
-   },
+   }
 
    this.revealCell = function(e) {
-      console.log('hello');
+      let cell = event.target
+      let mineObj = cell.childNodes[0];
+      let containsMine = (typeof(event.target.childNodes[0]) !== 'undefined');
+      event.target.style.backgroundColor = 'white';
+
+      if (containsMine) {
+         mineObj.style.display = 'block';
+         this.showMines();
+      } else {
+         return
+      }
    }
 
    this.placeMines = function() {
       // Eventually will be user inputed
       let cellsToPlaceMines = document.getElementsByClassName('cell');
-
       // Randomly generating nums to place mines
-      let i = 0;
+      let mineIteration = 0;
 
-      while (i < 5) {
-         randomCellNum = Math.floor(Math.random() * cellsToPlaceMines.length);
-         let newMine = document.createElement('div');
-         newMine.className = 'mine';
+      while (mineIteration < 5) {
+         let x = Math.floor((Math.random() * this.widthOfBoard));
+         let y = Math.floor((Math.random() * this.heightOfBoard));
 
-         let isCellEmpty = cellsToPlaceMines[randomCellNum].hasChildNodes();
-
-         if (isCellEmpty) {
-            continue;
-         } else {
-            cellsToPlaceMines[randomCellNum].appendChild(newMine);
-            i += 1;
+         if (map[x][y].includes(' ')) {
+            map[x][y] = ['m'];
+            mineIteration ++;
+            console.log(mineIteration);
          }
       }
+      this.createBoard();
    }
+
 }
 
 var easy = new Mineboard(7, 7, 5);
-easy.create();
+easy.createBoardGrid();
 
-// Generate the board with a function
-   // Number of mines will eventually be a user input
-   // Randomly place those mines within the board
-      // Perhaps query select all of the divs with class 'cell' and randomly select those to place mines
+// Generate a nested array map with several options: ' ', 'B'
+   // Perhaps place all of the ' ' first and then use the place mines function to while loop through it and randomly place mines
+// Then, go back and add the numebrs inside of that array based on the bombs around it
+// Loop through the map array and create the board based on what is in the map
+// Click event will check whether it is a bomb, empty or number
+// If bomb display all the bombs
+// If number just display the number
+// If empty, ?(loop) through the areas around it and stop when there is a number
