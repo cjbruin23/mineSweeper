496d56434ea72e6b15717bc1726bca366c2043dc
Colin Bruin <colinjb23@gmail.com>
2018-03-16 15:46:59 -0400

    attempting to count the numbers of mines around cells

--- sweep.js
+++ sweep.js
@@ -32,21 +32,20 @@ function Mineboard(rows, cols, mines) {
          for (let l = 0; l < map[i].length; l++) {
             let cell = document.createElement('div');
             cell.className = 'cell';
-            if (!map[i][l].includes(' ')) {
-               cell.id = 'mine';
-            }
+            if ( map[i][l].includes('m') ) { cell.className = 'cell mine'; }
 
             cell.addEventListener("click", function(e) {self.revealCell();}, false);
             row.appendChild(cell);
          };
       };
       document.body.appendChild(board);
-   }
+   };
 
    this.revealCell = function(e) {
       let cell = event.target;
+      let hasMineClass = cell.classList.contains('mine');
       cell.style.backgroundColor = 'white';
-      if (cell.id === 'mine') {
+      if (hasMineClass) {
          cell.style.backgroundImage = "url('mine.png')";
          this.mineExplosion();
       }
@@ -68,9 +67,54 @@ function Mineboard(rows, cols, mines) {
             map[x][y] = ['m'];
             mineIteration ++;
          };
-      }
+      };
+      this.placeNumbersArray();
       this.createBoard();
+   };
+
+   this.countMines = function(row, col, rowDir, colDir) {
+
+      console.log(row, col, rowDir, colDir);
+      let inBoundRow = ((row+rowDir >= 0) && (row+rowDir < map.length))
+      let inBoundCol = ((col+colDir >= 0) && (col+colDir < map[0].length))
+      console.log(map[4][1])
+      // if (inBoundRow && inBoundCol) {
+      //    // while (true) {
+      //    //    if (map[row+rowDir][col+colDir].includes('m')) {
+      //    //       console.log(true);
+      //    //       rowDir += rowDir;
+      //    //       colDir += colDir;
+      //    //    } else {
+      //    //       break;
+      //    //    }
+      //    }
+      // }
    }
+
+   this.placeNumbersArray = function() {
+      let finalCount;
+
+      for (let i = 0; i < map.length; i++) {
+         for (let l = 0; l < map[i].length; l++) {
+            // Up and to the right
+            this.countMines(i, l, -1, 1);
+            // Up
+            this.countMines(i, l, -1,0);
+            // Down
+            this.countMines(i, l, 1,0);
+            // Right
+            this.countMines(i, l, 0,1);
+            //Left
+            this.countMines(i, l, 0,-1);
+            //Up and to the left
+            this.countMines(i, l, -1, -1);
+            //Down and to the right
+            this.countMines(i, l, 1, 1);
+            // Down and to the Left
+            this.countMines(i, l, 1,-1);
+         };
+      };
+   };
 }
 
 var easy = new Mineboard(7, 7, 5);
